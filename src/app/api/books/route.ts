f3e3f7f94016cd89f4d576c2817a33c3450ff395
Ali Muhammad <ali9796408@gmail.com>
2023-04-17 15:46:39 +0500

    bug fix

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,22 +1,34 @@
+import type { Book } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 import prisma from '@/db';
 
 export async function GET() {
-  const books = prisma.book.findMany();
-  return NextResponse.json(books);
+  try {
+    const books = await prisma.book.findMany();
+    return NextResponse.json(books);
+  } catch (error) {
+    return NextResponse.json({ message: 'Error fetching books', error });
+  }
 }
 
 export async function POST(request: Request) {
   // eslint-disable-next-line unused-imports/no-unused-vars
   // const { name, author, isbn, type, price, currentStock, available } =
   //   await request.json();
-  const res = await request.json();
+  // const res = await request.json();
 
-  const book = await prisma.book.create({
-    data: res,
-  });
-  return NextResponse.json({
-    book,
-  });
+  // const book = await prisma.book.create({
+  //   data: res,
+  // });
+  // return NextResponse.json({
+  //   book,
+  // });
+  const newBook: Book = request.body;
+  try {
+    const createdBook = await prisma.book.create({ data: newBook });
+    return NextResponse.json(createdBook);
+  } catch (error) {
+    return NextResponse.json({ message: 'Error creating user', error });
+  }
 }
